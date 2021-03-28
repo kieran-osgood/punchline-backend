881a5fe68d814e2d9690d6556144bbef3379fc6b
Kieran Osgood <kieranbosgood@gmail.com>
2021-03-28 11:49:57 +0100

    feat(ApplicationDbContext): add new migration

---
+++ GraphQL/Migrations/20210328095252_ModifyFieldsOnJokesAndCategories.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraphQL.Migrations
+{
+    public partial class ModifyFieldsOnJokesAndCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Jokes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Jokes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Jokes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Jokes");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Jokes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Jokes",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
