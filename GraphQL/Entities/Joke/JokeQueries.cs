09e165a51ed4e694e980a27a9d4648c6d8ee0575
Kieran Osgood <kieranbosgood@gmail.com>
2021-04-03 17:57:53 +0100

    feat: implement joke length filtering

--- GraphQL/Entities/Joke/JokeQueries.cs
+++ GraphQL/Entities/Joke/JokeQueries.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using GraphQL.Data;
 using GraphQL.Extensions;
@@ -5,19 +6,33 @@ using HotChocolate;
 using HotChocolate.Data;
 using HotChocolate.Types;
 using HotChocolate.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQL.Entities.Joke
 {
     [ExtendObjectType("Query")]
     public class JokeQueries
     {
+        public enum JokeLength
+        {
+            Small = 175,
+            Medium = 400,
+            Large = 5000
+        }
+
         [UseApplicationDbContext]
         [Authorize]
         [UsePaging]
         [UseFiltering]
         [UseSorting]
         public IQueryable<Data.Joke> GetJokes(
-            [ScopedService] ApplicationDbContext context) =>
-            context.Jokes;
+            [ScopedService] ApplicationDbContext context,
+            JokeLength jokeLength)
+        {
+            var length = (int) jokeLength;
+            return from j in context.Jokes
+                where j.Body.Length < length
+                select j;
+        }
     }
 }
