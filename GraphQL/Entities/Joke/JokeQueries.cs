43849c390a37e4085a8b2cea2ccfd3b2c0d86f52
Kieran Osgood <kieranbosgood@gmail.com>
2021-04-09 17:42:58 +0100

    chore(JokeQueries): add code comments for documentation/notes

--- GraphQL/Entities/Joke/JokeQueries.cs
+++ GraphQL/Entities/Joke/JokeQueries.cs
@@ -25,9 +25,11 @@ namespace GraphQL.Entities.Joke
             [Service] ICategoryRepository categoryRepository,
             JokeLength jokeLength = JokeLength.Medium)
         {
+            // Casting to variable because of upstream bug: https://github.com/npgsql/efcore.pg/issues/1281
             var length = (int) jokeLength;
             var categoryIds = await categoryRepository.GetCategoryIdsByUserUid(userUid);
 
+            // Monitor this for performance - nested NOT (EXIST) - top level join may be better
             return from j in context.Jokes
                 where j.Body.Length < length
                 from c in j.Categories.Where(x => categoryIds.Count <= 0 || categoryIds.Contains(x.Id))
