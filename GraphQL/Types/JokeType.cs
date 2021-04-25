d2512ae6e7807005056a404519782ef55a6dc64f
Kieran Osgood <kieranbosgood@gmail.com>
2021-04-25 19:40:19 +0100

    feat(JokeType): add resolver for categories

--- GraphQL/Types/JokeType.cs
+++ GraphQL/Types/JokeType.cs
@@ -1,11 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using GraphQL.Data;
 using GraphQL.DataLoader;
+using GraphQL.Extensions;
+using GreenDonut;
+using HotChocolate;
 using HotChocolate.Resolvers;
 using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQL.Types
 {
-    public class JokeType: ObjectType<Joke>
+    public class JokeType : ObjectType<Joke>
     {
         protected override void Configure(IObjectTypeDescriptor<Joke> descriptor)
         {
@@ -15,6 +25,29 @@ namespace GraphQL.Types
                 .ResolveNode((ctx, id) =>
                     ctx.DataLoader<JokeByIdDataLoader>().LoadAsync(id, ctx.RequestAborted));
 
-    }
+            descriptor
+                .Field(t => t.Categories)
+                .ResolveWith<JokeResolvers>(t => t.GetCategoriesAsync(default!, default!, default!, default!))
+                .UseDbContext<ApplicationDbContext>()
+                .Name("categories");
+        }
+
+        private class JokeResolvers
+        {
+            public async Task<IEnumerable<Category>> GetCategoriesAsync(
+                Joke joke,
+                CategoryByIdDataLoader dataLoader,
+                [ScopedService] ApplicationDbContext dbContext,
+                CancellationToken cancellationToken)
+            {
+                var ids = await (
+                        from j in dbContext.Jokes
+                        where j.Id == joke.Id
+                        select j.Categories.Select(x=>x.Id).ToList())
+                    .FirstOrDefaultAsync(cancellationToken);
+
+                return await dataLoader.LoadAsync(ids, cancellationToken);
+            }
+        }
     }
 }
