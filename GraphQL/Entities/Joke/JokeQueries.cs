f4462744f00e8bdc199d673674ce222067feb00a
Kieran Osgood <kieranbosgood@gmail.com>
2021-09-05 10:09:41 +0100

    fix(JokeQueries): bug with querying length and filtering jokehistory and blockedCategoryIds

--- GraphQL/Entities/Joke/JokeQueries.cs
+++ GraphQL/Entities/Joke/JokeQueries.cs
@@ -4,49 +4,59 @@ using System.Linq;
 using System.Threading.Tasks;
 using GraphQL.Data;
 using GraphQL.Extensions;
-using GraphQL.Repositories.Category;
 using GraphQL.Static;
 using HotChocolate;
 using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Data;
 using HotChocolate.Types;
 using HotChocolate.Types.Relay;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQL.Entities.Joke
 {
     [ExtendObjectType(ObjectTypes.Query)]
     public class JokeQueries
     {
-        // [Authorize]
+        [Authorize]
         [UseApplicationDbContext]
         [UsePaging]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Data.Joke> GetJokes(
+        public async Task<IQueryable<Data.Joke>> GetJokes(
             [ScopedService] ApplicationDbContext context,
             [GlobalState(GlobalStates.HttpContext.UserUid)]
             string? userUid,
-            [Service] ICategoryRepository categoryRepository,
             [ID(nameof(Data.Joke))] int? deepLinkedJokeId,
             JokeLength jokeLength = JokeLength.Medium,
-            [ID(nameof(Data.Category))] List<int> blockedCategoryIds = default!)
+            [ID(nameof(Data.Category))] List<int>? blockedCategoryIds = default!)
         {
             // Casting to variable because of upstream bug: https://github.com/npgsql/efcore.pg/issues/1281
             var length = (int) jokeLength;
-            
-            // TODO Monitor this for performance - nested NOT (EXIST) - top level join may be better
+
+            var user = await (from u in context.Users where u.FirebaseUid == userUid select u).FirstOrDefaultAsync();
+            if (user is null)
+            {
+                throw new Exception("Oops");
+            }
+
             var jokes = (from j in context.Jokes
-                where j.Body.Length < length
-                from c in j.Categories.Where(x => blockedCategoryIds.Count <= 0 || !blockedCategoryIds.Contains(x.Id))
-                where !context.UserJokeHistory.Any(x => x.JokeId == j.Id)
+                where j.Body.Length <= length
+                where !context.UserJokeHistory.Where(x => x.UserId == user.Id).Any(x => x.JokeId == j.Id)
                 select j);
             
+            if (blockedCategoryIds != null)
+            {
+                jokes = (from j in jokes
+                    from c in j.Categories.Where(x => blockedCategoryIds != null && (blockedCategoryIds.Count <= 0 || !blockedCategoryIds.Contains(x.Id)))
+                    select j);
+            }
+
             if (deepLinkedJokeId.HasValue)
             {
                 var deepLinkedJokeQueryable = from j in context.Jokes
                     where j.Id == deepLinkedJokeId
                     select j;
-                return deepLinkedJokeQueryable.Concat(jokes).Distinct().OrderBy(x=>x.Id != deepLinkedJokeId);
+                return deepLinkedJokeQueryable.Concat(jokes).Distinct().OrderBy(x => x.Id != deepLinkedJokeId);
             }
 
             return jokes;
