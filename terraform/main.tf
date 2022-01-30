9bd0a77192920c6853f0f8205019ef3d90a247e8
Kieran Osgood <kieran.osgood@and.digital>
2022-01-30 10:32:26 +0000

    feat: import punchline-backend-cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,21 @@
 // Provider configuration
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 3.0"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
- 
+
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_ecs_cluster" "punchline-backend-cluster" {
+  name = "punchline-backend-cluster"
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
 }
