a4786772566be418e76031fd19b2c5178c037ee8
Kieran Osgood <kieran.osgood@and.digital>
2022-01-30 10:07:29 +0000

    feat: perform terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+// Provider configuration
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+}
+ 
+provider "aws" {
+ region = "us-east-1"
+}
